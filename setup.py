6a20ae0030508949cf88cbd57a67dcbc01b86d6d
abhinava-saha <abhinava_saha@gmail.com>
2023-04-15 12:48:52 +0530

    setup.py and requirement.txt

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def get_requirements(file_path:str)->List['str']:
     return requirements
 
 setup(
-name='studmarks',
+name='ipl-winner-prediction',
 version='0.0.1',
 author='abhinava-saha',
 author_email='abhinava.saha@outlook.com',
